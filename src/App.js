d7a427c05d071d3fb6cbb314ef8043c9d652e514
yungdenzel <oyomafia@gmail.com>
2019-10-30 19:00:26 +0100

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Note from './components/Note'
 import './App.css';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      noteText: '',
+      notes: [],
+    }
+  }
+    updateNoteText(noteText){
+     this.setState({noteText: noteText.target.value}) 
+    }
+    addNote() {
+      if (this.state.noteText === '') {return}
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({noteText: ''});
+      this.textInput.focus();
+    }
 
-function App() {
+    handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+          let notesArr = this.state.notes;
+          notesArr.push(this.state.noteText);
+          this.setState({noteText: ''});
+      }
+    }
+  
+  deleteNote(index){
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({notes: notesArr})
+  }
+render() {
+  let notes = this.state.notes.map((vol, key) => {
+    return <Note key={key} text={vol}
+            deleteMethod={ () => this.deleteNote(key)}/>
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Container">
+     <div className="header">React TodoList </div>
+      < input type = "text"ref = {((input) => {this.textInput =input})}
+     className = "textInput" value ={this.state.noteText}onChange={noteText => this.updateNoteText(noteText)}
+     onKeyPress ={this.handleKeyPress.bind(this)}/>     
+     <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+      {notes}
+     </div>
   );
 }
-
+}
 export default App;
