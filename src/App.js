3fe5da9bd853e61bbbc93e4625de9301c4ef69b7
yungdenzel <oyomafia@gmail.com>
2019-11-13 03:47:19 +0100

    cssing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
+
 import Note from './components/Note'
 import './App.css';
+
+const styles = {
+  outer: {
+    borderRadius: 5,
+    border: '2px solid gray',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+  },
+  inner: checked => ({
+    borderRadius: '50%',
+    height: 28,
+    width: 28,
+    backgroundColor: checked ? 'red' : 'transparent',
+    margin: 1,
+    transition: 'background-color 0.2s ease',
+  })
+}
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       noteText: '',
       notes: [],
+      checked: false,
     }
   }
     updateNoteText(noteText){
      this.setState({noteText: noteText.target.value}) 
     }
+   
     addNote() {
       if (this.state.noteText === '') {return}
       let notesArr = this.state.notes;
@@ -33,19 +55,30 @@ class App extends Component {
     notesArr.splice(index, 1);
     this.setState({notes: notesArr})
   }
+  editnote(){
+    // var todo = this.state.notes;
+    // var editodo = notes[i];
+
+   
+
+  }
+
 render() {
   let notes = this.state.notes.map((vol, key) => {
     return <Note key={key} text={vol}
             deleteMethod={ () => this.deleteNote(key)}/>
   })
   return (
-    <div className="Container">
-     <div className="header">React TodoList </div>
-      < input type = "text"ref = {((input) => {this.textInput =input})}
-     className = "textInput" value ={this.state.noteText}onChange={noteText => this.updateNoteText(noteText)}
+    <div className="fluid-container bg-light w-50 mx-auto gron">
+      <div className="header h1 text-center mt-md-3">React TodoList </div>
+      <div className="card mt-md-3">
+      < input  type= "text"ref = {((input) => {this.textInput =input})}
+     className = "textInput" placeholder="write your todo here" value ={this.state.noteText}onChange={noteText => this.updateNoteText(noteText)}
      onKeyPress ={this.handleKeyPress.bind(this)}/>     
-     <div className="btn" onClick={this.addNote.bind(this)}>+</div>
-      {notes}
+        <div className="btn btn-success " onClick={this.addNote.bind(this)}>Enter</div>
+         <div style={styles} className =" list-group list-group-flush text-center notes mt-md-3"/> 
+        {notes}  
+     </div>
      </div>
   );
 }
